8500df0dadcc6b646c66c14779f1493fec7ba6b1
nirajpandkar <niraj.pandkar@gmail.com>
2016-08-02 16:07:13 +0530

    Changed aesthetics

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var chalk = require('chalk');
 var tips = require('./tips.json');
 var uniqueRandomArray = require('unique-random-array');
 
-var randomGitTip = uniqueRandomArray(tips);
-var randomTip = randomGitTip();
-var randomTipTitle = randomTip.title;
-var randomTipTip = randomTip.tip;
-console.log("***************************");
-console.log("* Git Tip of the Terminal *");
-console.log("***************************");
-console.log(chalk.bold.green(randomTipTitle));
-console.log(chalk.bold.cyan(randomTipTip));
+var randomGitTip = uniqueRandomArray(tips); //returns a function
+var randomTip = randomGitTip(); //returns a random object(title,tip) from tips.json
+var randomTipTitle = randomTip.title;   //Title of the tip
+var randomTipCommand = randomTip.tip;       //Actual command
+console.log(chalk.bold.yellow("***************************"));
+console.log(chalk.bold.yellow("* Git Tip of the Terminal *"));
+console.log(chalk.bold.yellow("***************************"));
+console.log(chalk.bold.cyan(randomTipTitle));
+console.log(chalk.bold.green(randomTipCommand));
